4a1f879485e6d973fdcfbd9db97dd4a2619a2fd5
adarshkushwah21111 <adarsh21111@iiitd.ac.in>
2022-03-13 22:16:19 +0530

    Know more screen added.

--- app/src/main/java/com/example/mcproject/knowMoreScreen.java
+++ app/src/main/java/com/example/mcproject/knowMoreScreen.java
@@ -2,27 +2,28 @@ package com.example.mcproject;
 
 import android.os.Bundle;
 
-import androidx.cardview.widget.CardView;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.daimajia.androidanimations.library.Techniques;
-import com.daimajia.androidanimations.library.YoYo;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link homeScreen#newInstance} factory method to
+ * Use the {@link knowMoreScreen#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class homeScreen extends Fragment {
-    TextView tv;
-    CardView knowMore;
+public class knowMoreScreen extends Fragment {
+    ImageView background;
+    TextView title;
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -32,7 +33,7 @@ public class homeScreen extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public homeScreen() {
+    public knowMoreScreen() {
         // Required empty public constructor
     }
 
@@ -42,11 +43,11 @@ public class homeScreen extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment homeScreen.
+     * @return A new instance of fragment knowMoreScreen.
      */
     // TODO: Rename and change types and number of parameters
-    public static homeScreen newInstance(String param1, String param2) {
-        homeScreen fragment = new homeScreen();
+    public static knowMoreScreen newInstance(String param1, String param2) {
+        knowMoreScreen fragment = new knowMoreScreen();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -67,19 +68,9 @@ public class homeScreen extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =inflater.inflate(R.layout.fragment_home_screen, container, false);
-        knowMore = view.findViewById(R.id.cardView5);
-        knowMore.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getActivity().getSupportFragmentManager()
-                             .beginTransaction()
-                             .replace(R.id.mainMenuContainer,new knowMoreScreen())
-                             .addToBackStack(null)
-                             .commit();
-            }
-        });
-
+        View view =  inflater.inflate(R.layout.fragment_know_more_screen, container, false);
+        background = view.findViewById(R.id.knowmoreScreenimageView);
+        background.setBackgroundResource(R.drawable.lifelinebackground);
         return view;
     }
 }
