8e22483480cbcee44804c4a53ccee537e7df3345
Shubham Rana <rana11shubham@gmail.com>
2022-03-21 00:10:31 +0530

    'Added_Main'

--- app/src/main/java/com/example/mcproject/mainMenuActivity.java
+++ app/src/main/java/com/example/mcproject/mainMenuActivity.java
@@ -1,15 +1,20 @@
 package com.example.mcproject;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.GestureDetector;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
@@ -17,57 +22,75 @@ public class mainMenuActivity extends AppCompatActivity {
     BottomNavigationView bmv;
     Boolean menuSet=true;
     LottieAnimationView lottieAnimationView;
+    MeowBottomNavigation meowBottomNavigation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
         getSupportFragmentManager().beginTransaction().replace(R.id.mainMenuContainer,new homeScreen()).commit();
 
-        bmv = findViewById(R.id.bottomNavigationView);
-        bmv.setItemIconTintList(null);                  //used for showing original icon color in menu
 
+        meowBottomNavigation = findViewById(R.id.bottomNavigation);
 
-        bmv.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @SuppressLint("NonConstantResourceId")
+        meowBottomNavigation.add(new MeowBottomNavigation.Model(1, R.drawable.ic__home_new));
+        meowBottomNavigation.add(new MeowBottomNavigation.Model(2, R.drawable.ic__profile_new));
+        meowBottomNavigation.add(new MeowBottomNavigation.Model(3, R.drawable.ic__settings_new));
+
+        meowBottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
-                switch (id){
-                    case R.id.home:
-                        //Toast.makeText(getApplicationContext(), "Home Pressed", Toast.LENGTH_SHORT).show();
+            public void onClickItem(MeowBottomNavigation.Model item) {
+                switch (item.getId()){
+                    case 1:
                         if(!menuSet){
-                            replacefragment();
+                            generateHomeFragment();
                         }
-                        return true;
-                    case R.id.profile:
-                        //Toast.makeText(getApplicationContext(), "Profile Pressed", Toast.LENGTH_SHORT).show();
-                        replacefragmentTwo();
+                        break;
+                    case 2:
+                        generateProfileFragment();
                         menuSet=false;
-                        return true;
-                    case R.id.settings:
-                        Toast.makeText(getApplicationContext(), "Settings Pressed", Toast.LENGTH_SHORT).show();
-                        Intent i=new Intent(mainMenuActivity.this,CovidCasesActivity.class);
-                         startActivity(i);
+                        break;
+                    case 3:
                         menuSet=false;
-                        return true;
+                        break;
+                    default: throw new IllegalStateException("Unexpected value: " + item.getId());
                 }
-                return false;
             }
         });
-//        lottieAnimationView=findViewById(R.id.knowmore);
-//        lottieAnimationView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent i=new Intent(mainMenuActivity.this,CovidCasesActivity.class);
-//                startActivity(i);
-//                finish();
-//            }
-//        });
+
+        meowBottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
+            @Override
+            public void onShowItem(MeowBottomNavigation.Model item) {
+                String name;
+                switch (item.getId()) {
+                    case 1:
+                        name = "Home";
+                        break;
+                    case 2:
+                        name = "Profile";
+                        break;
+                    case 3:
+                        name = "Settings";
+                        break;
+                    default:
+                        name = "";
+                        break;
+                }
+            }
+        });
+        meowBottomNavigation.setOnReselectListener(new MeowBottomNavigation.ReselectListener() {
+            @Override
+            public void onReselectItem(MeowBottomNavigation.Model item) {
+                Toast.makeText(getApplicationContext(), "Already Pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
+        meowBottomNavigation.show(1,true);
     }
-    private void replacefragment(){
-        getSupportFragmentManager().beginTransaction().replace(R.id.mainMenuContainer,new homeScreen()).commit();
+    private void generateHomeFragment(){
+        getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.enter_from_right,R.anim.exit_to_right,R.anim.enter_from_right,R.anim.exit_to_right).replace(R.id.mainMenuContainer,new homeScreen()).commit();
     }
-    private void replacefragmentTwo(){
-        getSupportFragmentManager().beginTransaction().replace(R.id.mainMenuContainer,new profileScreen()).commit();
+    private void generateProfileFragment(){
+        getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.enter_from_right,R.anim.exit_to_right,R.anim.enter_from_right,R.anim.exit_to_right).replace(R.id.mainMenuContainer,new profileScreen()).commit();
     }
 }
