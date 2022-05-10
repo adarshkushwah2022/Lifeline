1a58410f3b1f30b5e9cbfee0fb472265226948ec
akashrwt7 <rawatakash535@gmail.com>
2022-05-11 00:44:16 +0530

    Commit by akash

---
+++ app/src/main/java/com/example/mcproject/sendNotification/Data.java
@@ -0,0 +1,17 @@
+package com.example.mcproject.sendNotification;
+
+public class Data {
+    String Name;
+
+    public Data(String name) {
+        this.Name = name;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        this.Name = name;
+    }
+}
